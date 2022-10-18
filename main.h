ddc40e51fd36d1066d521bce22798685455d5417
Emyx3D <emixjasper@gmail.com>
2022-10-18 10:42:23 +0100

    edit

--- main.h
+++ main.h
@@ -34,10 +34,8 @@ int print_integer(va_list);
 int print_number(va_list);
 
 int _puts(char *string);
-int printc(char *format, va_list);
 int printstr(char *format, va_list);
 int (*driver(char *format))(char *format, va_list);
-int _printf(char *format, ...);
 int printint(char *format, va_list pa);
 int integer(int number);
 int contadordigit(int number);
