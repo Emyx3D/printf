a7b2da447a9221b1f2d4064978babdf8784da740
Emyx3D <emixjasper@gmail.com>
2022-10-18 10:38:55 +0100

    printf

--- main.h
+++ main.h
@@ -33,4 +33,19 @@ int print_percent(va_list);
 int print_integer(va_list);
 int print_number(va_list);
 
+int _puts(char *string);
+int printc(char *format, va_list);
+int printstr(char *format, va_list);
+int (*driver(char *format))(char *format, va_list);
+int _printf(char *format, ...);
+int printint(char *format, va_list pa);
+int integer(int number);
+int contadordigit(int number);
+int _abs(int number);
+int printpercent(char *format, va_list pa);
+int printhex(char *format, va_list);
+int printHEX(char *format, va_list);
+int printocta(char *format, va_list);
+int print_unsign(char *format, va_list);
+
 #endif /*MAIN_H_*/
